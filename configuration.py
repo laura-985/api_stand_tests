fa6596fcc1441dd0889dbe3201c1f73fc8e27aff
laura  gonzalez <l.l.g.m9985@gmail.com>
2024-10-28 15:05:24 -0300

    mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = ' https://cnt-dc54f0e0-f7a2-44aa-a5e0-dea8d27cb35a.containerhub.tripleten-services.com'
+DOC_PATH = '/docs/'
+LOG_MAIN_PATH ='/api/logs/main/'
+USERS_TABLE_PATH = '/api/db/resources/user_model.csv'
+CREATE_USER_PATH = '/api/v1/users/'
+PRODUCTS_KITS_PATH = '/api/v1/products/kits/'
